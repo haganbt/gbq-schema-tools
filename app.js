6ec9c3b4b759582c6ef627d0e21591f3f6707aff
Ben Hagan <benhagan@gmail.com>
2013-09-03 15:52:49 +0100

    Use config port.

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var express = require('express')
 var app = express();
 
 app.configure(function () {
-  app.set('port', process.env.PORT || 3000);
+  app.set('port', config.port);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.favicon());
